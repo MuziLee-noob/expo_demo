305739b29ca78b1a2624a644552b57c511f12544
lijinhe <lijinhe@aisino.com>
2020-06-29 17:57:52 +0800

    init

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
+import MessagesScreen from "./app/screens/MessagesScreen";
+
+export default function App() {
+  return <MessagesScreen />;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
---
+++ app/components/AppText.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Text } from "react-native";
+import { StyleSheet, Platform } from "react-native";
+
+function AppText({ children, style }) {
+  return <Text style={[styles.text, style]}>{children}</Text>;
+}
+
+const styles = StyleSheet.create({
+  text: {
+    ...Platform.select({
+      ios: {
+        fontSize: 20,
+        fontFamily: "Avenir",
+      },
+      android: {
+        fontSize: 18,
+        fontFamily: "Roboto",
+      },
+    }),
+  },
+});
+
+export default AppText;
