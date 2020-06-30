d393a6e73250db7790a984b7ea9e7e22094d6dc5
lijinhe <lijinhe@aisino.com>
2020-06-30 16:01:47 +0800

    [add] CustomPicker

--- App.js
+++ App.js
@@ -1,10 +1,39 @@
-import React from "react";
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  TextInput,
+  Switch,
+} from "react-native";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ListingsScreen from "./app/screens/ListingsScreen";
+import Screen from "./app/components/Screen";
+import AppTextInput from "./app/components/AppTextInput";
+import AppPicker from "./app/components/AppPicker";
+
+const categories = [
+  { key: 1, label: "Furnitrue", value: 1 },
+  { key: 2, label: "Clothing", value: 2 },
+  { key: 3, label: "Cameras", value: 3 },
+];
 
 export default function App() {
-  return <ListingsScreen />;
+  const [category, setCategory] = useState();
+
+  return (
+    <Screen>
+      <AppPicker
+        selectedItem={category}
+        onSelectItem={(item) => setCategory(item)}
+        items={categories}
+        icon="apps"
+        placeholder="Category"
+      />
+      <AppTextInput icon="email" placeholder="Email" />
+    </Screen>
+  );
 }
 
 const styles = StyleSheet.create({
